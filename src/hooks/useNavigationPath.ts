a954a258890a3434cd5a4e00999a1a7083c1c186
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-07-03 19:41:57 +0300

    Run prettier(as it get out of sync in editor)

--- src/hooks/useNavigationPath.ts
+++ src/hooks/useNavigationPath.ts
@@ -3,8 +3,8 @@ export const useNavigationPath = () => {
   // in real life they can come from real navigation or storage
 
   return [
-    { label: 'dashbird.io', id: 'dashbird', href: "#dashbird" },
-    { label: 'US-EAST-1', id: 'us-east-1', href: "#us-east-1" },
-    { label: 'SQS', id: 'sqs', href: "#sqs" }
+    { label: 'dashbird.io', id: 'dashbird', href: '#dashbird' },
+    { label: 'US-EAST-1', id: 'us-east-1', href: '#us-east-1' },
+    { label: 'SQS', id: 'sqs', href: '#sqs' },
   ];
-}
+};
