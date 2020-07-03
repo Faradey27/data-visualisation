0da5d55465ed8b2bf8fff6fd9417907e0a9914f2
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-07-03 19:41:35 +0300

    Add Dashbird title to main page

--- src/components/DashboardTitle/DashboardTitle.tsx
+++ src/components/DashboardTitle/DashboardTitle.tsx
@@ -1,10 +1,18 @@
 import React, { memo } from 'react';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './DashboardTitle.module.scss';
 
 const DashboardTitle: React.FC<{}> = () => {
   return (
-    <div>
-      <div />
-      <h4>alerting-check-policy-conditions</h4>
+    <div className={styles.root}>
+      <FontAwesomeIcon
+        className={styles.icon}
+        icon={faExclamationCircle}
+        color="inherit"
+      />
+      <h1 className={styles.title}>alerting-check-policy-conditions</h1>
     </div>
   );
 };
