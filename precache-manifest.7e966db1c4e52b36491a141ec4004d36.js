cebd9945b3884f1fd28ea98ae134816a5391a2a6
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-07-03 22:29:12 +0300

    Updates

---
+++ precache-manifest.7e966db1c4e52b36491a141ec4004d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a083f64d961196467f850e8ed164a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "578e55dbb8223131988b",
+    "url": "/static/css/main.daffec0e.chunk.css"
+  },
+  {
+    "revision": "dc24aa706862708872ab",
+    "url": "/static/js/2.bdeba33e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdeba33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578e55dbb8223131988b",
+    "url": "/static/js/main.92bf52a4.chunk.js"
+  },
+  {
+    "revision": "0910205d97d36baa1017",
+    "url": "/static/js/runtime-main.4d949d8a.js"
+  }
+]);
